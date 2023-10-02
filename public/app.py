27fd4ca34509b3dd82b09f4cfbd0b6fcf0197286
Eng-Sillah <ibrahimssillah213@gmail.com>
2023-10-02 02:50:31 -0600

    ttt

--- public/app.py
+++ public/app.py
@@ -1,15 +1,33 @@
 from flask import Flask, request
+import os
 
 app = Flask(__name__)
 
-@app.route('/ussd', methods=['GET', 'POST'])
+@app.route('/', methods=['POST', 'GET'])
 def ussd_callback():
-    session_id = request.values.get("sessionId")
-    service_code = request.values.get("serviceCode")
-    phone_number = request.values.get("phoneNumber")
-    text = request.values.get("text")
+    session_id = request.values.get("sessionId", None)
+    service_code = request.values.get("*384*932454#", None)
+    phone_number = request.values.get("079932454", None)
+    text = request.values.get("text", "default")
 
-    # Your USSD logic goes here
+    if text == '':
+        response = "CON What would you want to check?\n"
+        response += "1. My Account\n"
+        response += "2. My phone number"
+    elif text == '1':
+        response = "CON Choose account information you want to view\n"
+        response += "1. Account number\n"
+        response += "2. Account balance"
+    elif text == '1*1':
+        accountNumber = "ACC1001"
+        response = "END Your account number is " + accountNumber
+    elif text == '1*2':
+        balance = "KES 10,000"
+        response = "END Your balance is " + balance
+    elif text == '2':
+        response = "END This is your phone number " + phone_number
+
+    return response
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
